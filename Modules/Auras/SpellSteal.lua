164e4fece43844f0cece5295105bda967296a8d9
Voopie <1324849+Voopie@users.noreply.github.com>
2021-05-23 12:48:39 +0300

    renaming

--- Modules/Auras/SpellSteal.lua
+++ Modules/Auras/SpellSteal.lua
@@ -10,7 +10,7 @@ local UpdateFontObject = S:GetNameplateModule('Handler').UpdateFontObject;
 
 -- Local Config
 local ENABLED, COUNTDOWN_ENABLED;
-local NAME_POSITION_V, NAME_TEXT_OFFSET_Y;
+local NAME_TEXT_POSITION_V, NAME_TEXT_OFFSET_Y;
 
 local StripesAurasSpellStealCooldownFont = CreateFont('StripesAurasSpellStealCooldownFont');
 local StripesAurasSpellStealCountFont    = CreateFont('StripesAurasSpellStealCountFont');
@@ -40,7 +40,7 @@ local function UpdateAnchor(unitframe)
         local showMechanicOnTarget = GetCVarBool(CVAR_RESOURCE_ON_TARGET) and 10 or 0;
 
         if ShouldShowName(unitframe) then
-            local offset = NAME_POSITION_V == 1 and (unitframe.name:GetLineHeight() + NAME_TEXT_OFFSET_Y + showMechanicOnTarget) or showMechanicOnTarget;
+            local offset = NAME_TEXT_POSITION_V == 1 and (unitframe.name:GetLineHeight() + NAME_TEXT_OFFSET_Y + showMechanicOnTarget) or showMechanicOnTarget;
             PixelUtil.SetPoint(unitframe.AurasSpellSteal, 'BOTTOM', unitframe.healthBar, 'TOP', 1, 2 + offset);
         else
             local offset = unitframe.BuffFrame:GetBaseYOffset() + (UnitIsUnit(unitframe.data.unit, 'target') and unitframe.BuffFrame:GetTargetYOffset() or 0.0);
@@ -168,10 +168,10 @@ function Module:Update(unitframe)
 end
 
 function Module:UpdateLocalConfig()
-    ENABLED            = O.db.auras_spellsteal_enabled;
-    COUNTDOWN_ENABLED  = O.db.auras_spellsteal_countdown_enabled;
-    NAME_POSITION_V    = O.db.name_text_position_v;
-    NAME_TEXT_OFFSET_Y = O.db.name_text_offset_y;
+    ENABLED              = O.db.auras_spellsteal_enabled;
+    COUNTDOWN_ENABLED    = O.db.auras_spellsteal_countdown_enabled;
+    NAME_TEXT_POSITION_V = O.db.name_text_position_v;
+    NAME_TEXT_OFFSET_Y   = O.db.name_text_offset_y;
 
     UpdateFontObject(StripesAurasSpellStealCooldownFont, O.db.auras_spellsteal_cooldown_font_value, O.db.auras_spellsteal_cooldown_font_size, O.db.auras_spellsteal_cooldown_font_flag, O.db.auras_spellsteal_cooldown_font_shadow);
     UpdateFontObject(StripesAurasSpellStealCountFont, O.db.auras_spellsteal_count_font_value, O.db.auras_spellsteal_count_font_size, O.db.auras_spellsteal_count_font_flag, O.db.auras_spellsteal_count_font_shadow);
