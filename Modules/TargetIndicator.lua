27471064adc064fa6b81d86ad2c51edaca33fad8
Voopie <1324849+Voopie@users.noreply.github.com>
2021-06-14 21:43:21 +0300

    local config & optimization

--- Modules/TargetIndicator.lua
+++ Modules/TargetIndicator.lua
@@ -12,11 +12,15 @@ local NP = S.NamePlates;
 
 -- Local Config
 local TARGET_INDICATOR_ENABLED, TARGET_GLOW_ENABLED, HOVER_GLOW_ENABLED;
+local TEXTURE, TARGET_INDICATOR_COLOR, TARGET_GLOW_COLOR;
+local SIZE, X_OFFSET, Y_OFFSET;
 
 local currentTargetGUID;
 
 local INDICATOR_TEXTURES = O.Lists.target_indicator_texture_path;
 local DEFAULT_INDICATOR = 1;
+
+local GLOW_TEXTURE = S.Media.Path .. 'Textures\\glow'
 
 local GLOW_UPDATE_INTERVAL = 0.1;
 
@@ -102,26 +106,17 @@ local function CreateTargetIndicator(unitframe)
     indicator:SetAllPoints(unitframe.healthBar);
 
     indicator.left = indicator:CreateTexture(nil, 'BORDER');
-    PixelUtil.SetPoint(indicator.left, 'RIGHT', unitframe.healthBar, 'LEFT', -(O.db.target_indicator_x_offset), O.db.target_indicator_y_offset);
-    PixelUtil.SetSize(indicator.left, O.db.target_indicator_size, O.db.target_indicator_size);
-    indicator.left:SetTexture(INDICATOR_TEXTURES[O.db.target_indicator_texture] or INDICATOR_TEXTURES[DEFAULT_INDICATOR]);
     indicator.left:Hide();
 
     indicator.right = indicator:CreateTexture(nil, 'BORDER');
-    PixelUtil.SetPoint(indicator.right, 'LEFT', unitframe.healthBar, 'RIGHT', O.db.target_indicator_x_offset, O.db.target_indicator_y_offset)
-    PixelUtil.SetSize(indicator.right, O.db.target_indicator_size, O.db.target_indicator_size);
-    indicator.right:SetTexture(INDICATOR_TEXTURES[O.db.target_indicator_texture] or INDICATOR_TEXTURES[DEFAULT_INDICATOR]);
     indicator.right:SetTexCoord(1, 0, 0, 1); -- Hor flip
     indicator.right:Hide();
-
-    indicator.left:SetVertexColor(unpack(O.db.target_indicator_color));
-    indicator.right:SetVertexColor(unpack(O.db.target_indicator_color));
 
     indicator.glowUp = indicator:CreateTexture(nil, 'BACKGROUND');
     PixelUtil.SetPoint(indicator.glowUp, 'TOPLEFT', unitframe.healthBar, 'TOPLEFT', 0, 6);
     PixelUtil.SetPoint(indicator.glowUp, 'TOPRIGHT', unitframe.healthBar, 'TOPRIGHT', 0, 6);
     PixelUtil.SetHeight(indicator.glowUp, 6);
-    indicator.glowUp:SetTexture(S.Media.Path .. 'Textures\\glow');
+    indicator.glowUp:SetTexture(GLOW_TEXTURE);
     indicator.glowUp:SetRotation(math_rad(180));
     indicator.glowUp:Hide();
 
@@ -129,11 +124,8 @@ local function CreateTargetIndicator(unitframe)
     PixelUtil.SetPoint(indicator.glowDown, 'TOPLEFT', unitframe.healthBar, 'BOTTOMLEFT', 0, 0);
     PixelUtil.SetPoint(indicator.glowDown, 'TOPRIGHT', unitframe.healthBar, 'BOTTOMRIGHT', 0, 0);
     PixelUtil.SetHeight(indicator.glowDown, 6);
-    indicator.glowDown:SetTexture(S.Media.Path .. 'Textures\\glow');
+    indicator.glowDown:SetTexture(GLOW_TEXTURE);
     indicator.glowDown:Hide();
-
-    indicator.glowUp:SetVertexColor(unpack(O.db.target_glow_color));
-    indicator.glowDown:SetVertexColor(unpack(O.db.target_glow_color));
 
     indicator:HookScript('OnUpdate', OnUpdate);
 
@@ -143,20 +135,20 @@ local function CreateTargetIndicator(unitframe)
 end
 
 local function UpdateStyle(unitframe)
-    PixelUtil.SetSize(unitframe.TargetIndicator.left, O.db.target_indicator_size, O.db.target_indicator_size);
-    PixelUtil.SetSize(unitframe.TargetIndicator.right, O.db.target_indicator_size, O.db.target_indicator_size);
+    PixelUtil.SetSize(unitframe.TargetIndicator.left, SIZE, SIZE);
+    PixelUtil.SetSize(unitframe.TargetIndicator.right, SIZE, SIZE);
 
-    PixelUtil.SetPoint(unitframe.TargetIndicator.left, 'RIGHT', unitframe.healthBar, 'LEFT', -(O.db.target_indicator_x_offset), O.db.target_indicator_y_offset);
-    PixelUtil.SetPoint(unitframe.TargetIndicator.right, 'LEFT', unitframe.healthBar, 'RIGHT', O.db.target_indicator_x_offset, O.db.target_indicator_y_offset);
+    PixelUtil.SetPoint(unitframe.TargetIndicator.left, 'RIGHT', unitframe.healthBar, 'LEFT', -(X_OFFSET), Y_OFFSET);
+    PixelUtil.SetPoint(unitframe.TargetIndicator.right, 'LEFT', unitframe.healthBar, 'RIGHT', X_OFFSET, Y_OFFSET);
 
-    unitframe.TargetIndicator.left:SetTexture(INDICATOR_TEXTURES[O.db.target_indicator_texture] or INDICATOR_TEXTURES[DEFAULT_INDICATOR]);
-    unitframe.TargetIndicator.right:SetTexture(INDICATOR_TEXTURES[O.db.target_indicator_texture] or INDICATOR_TEXTURES[DEFAULT_INDICATOR]);
+    unitframe.TargetIndicator.left:SetTexture(TEXTURE);
+    unitframe.TargetIndicator.right:SetTexture(TEXTURE);
 
-    unitframe.TargetIndicator.left:SetVertexColor(unpack(O.db.target_indicator_color));
-    unitframe.TargetIndicator.right:SetVertexColor(unpack(O.db.target_indicator_color));
+    unitframe.TargetIndicator.left:SetVertexColor(unpack(TARGET_INDICATOR_COLOR));
+    unitframe.TargetIndicator.right:SetVertexColor(unpack(TARGET_INDICATOR_COLOR));
 
-    unitframe.TargetIndicator.glowUp:SetVertexColor(unpack(O.db.target_glow_color));
-    unitframe.TargetIndicator.glowDown:SetVertexColor(unpack(O.db.target_glow_color));
+    unitframe.TargetIndicator.glowUp:SetVertexColor(unpack(TARGET_GLOW_COLOR));
+    unitframe.TargetIndicator.glowDown:SetVertexColor(unpack(TARGET_GLOW_COLOR));
 end
 
 local function UpdateMouseoverUnit()
@@ -196,6 +188,23 @@ function Module:UpdateLocalConfig()
     TARGET_INDICATOR_ENABLED = O.db.target_indicator_enabled;
     TARGET_GLOW_ENABLED      = O.db.target_glow_enabled;
     HOVER_GLOW_ENABLED       = O.db.hover_glow_enabled;
+
+    SIZE     = O.db.target_indicator_size;
+    X_OFFSET = O.db.target_indicator_x_offset;
+    Y_OFFSET = O.db.target_indicator_y_offset;
+    TEXTURE  = INDICATOR_TEXTURES[O.db.target_indicator_texture] or INDICATOR_TEXTURES[DEFAULT_INDICATOR];
+
+    TARGET_INDICATOR_COLOR = TARGET_INDICATOR_COLOR or {};
+    TARGET_INDICATOR_COLOR[1] = O.db.target_indicator_color[1];
+    TARGET_INDICATOR_COLOR[2] = O.db.target_indicator_color[2];
+    TARGET_INDICATOR_COLOR[3] = O.db.target_indicator_color[3];
+    TARGET_INDICATOR_COLOR[4] = O.db.target_indicator_color[4] or 1;
+
+    TARGET_GLOW_COLOR = TARGET_GLOW_COLOR or {};
+    TARGET_GLOW_COLOR[1] = O.db.target_glow_color[1];
+    TARGET_GLOW_COLOR[2] = O.db.target_glow_color[2];
+    TARGET_GLOW_COLOR[3] = O.db.target_glow_color[3];
+    TARGET_GLOW_COLOR[4] = O.db.target_glow_color[4] or 1;
 end
 
 function Module:StartUp()
