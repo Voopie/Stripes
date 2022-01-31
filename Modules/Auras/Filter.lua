465dc0749c620e0e081ae424571a30cac4357077
Voopie <1324849+Voopie@users.noreply.github.com>
2022-01-31 07:44:40 +0300

    spaces

--- Modules/Auras/Filter.lua
+++ Modules/Auras/Filter.lua
@@ -51,14 +51,14 @@ local function FilterShouldShowBuff(self, name, spellId, caster, nameplateShowPe
             return false;
         end
 
-		if O.db.auras_blacklist[name] and O.db.auras_blacklist[name].enabled then
-			blacklistAurasNameCache[name] = spellId;
-			return false;
-		end
+        if O.db.auras_blacklist[name] and O.db.auras_blacklist[name].enabled then
+            blacklistAurasNameCache[name] = spellId;
+            return false;
+        end
 
         if spellId and O.db.auras_blacklist[spellId] and O.db.auras_blacklist[spellId].enabled then
-			blacklistAurasNameCache[name] = spellId;
-			return false;
+            blacklistAurasNameCache[name] = spellId;
+            return false;
         end
     end
 
@@ -70,96 +70,96 @@ local function FilterShouldShowBuff(self, name, spellId, caster, nameplateShowPe
 end
 
 local function UpdateBuffs(self, unit, filter, showAll)
-	if not self.isActive then
-		for i = 1, BUFF_MAX_DISPLAY do
-			if self.buffList[i] then
-				self.buffList[i]:Hide();
-			else
-				break;
-			end
-		end
+    if not self.isActive then
+        for i = 1, BUFF_MAX_DISPLAY do
+            if self.buffList[i] then
+                self.buffList[i]:Hide();
+            else
+                break;
+            end
+        end
 
-		return;
-	end
+        return;
+    end
 
-	self.unit   = unit;
-	self.filter = filter;
+    self.unit   = unit;
+    self.filter = filter;
 
-	self:UpdateAnchor();
+    self:UpdateAnchor();
 
-	if filter == 'NONE' then
-		for _, buff in ipairs(self.buffList) do
-			buff:Hide();
-		end
-	else
-		local buffIndex = 1;
-		local index = 1;
+    if filter == 'NONE' then
+        for _, buff in ipairs(self.buffList) do
+            buff:Hide();
+        end
+    else
+        local buffIndex = 1;
+        local index = 1;
         local _, name, texture, count, duration, expirationTime, caster, nameplateShowPersonal, spellId, nameplateShowAll;
 
-		AuraUtil_ForEachAura(unit, filter, BUFF_MAX_DISPLAY, function(...)
-			name, texture, count, _, duration, expirationTime, caster, _, nameplateShowPersonal, spellId, _, _, _, nameplateShowAll = ...;
+        AuraUtil_ForEachAura(unit, filter, BUFF_MAX_DISPLAY, function(...)
+            name, texture, count, _, duration, expirationTime, caster, _, nameplateShowPersonal, spellId, _, _, _, nameplateShowAll = ...;
 
-			if FilterShouldShowBuff(self, name, spellId, caster, nameplateShowPersonal, nameplateShowAll or showAll) then
-				if not self.buffList[buffIndex] then
-					self.buffList[buffIndex] = CreateFrame('Frame', nil, self, 'NameplateBuffButtonTemplate');
-					self.buffList[buffIndex]:SetMouseClickEnabled(false);
-					self.buffList[buffIndex].layoutIndex = buffIndex;
-				end
+            if FilterShouldShowBuff(self, name, spellId, caster, nameplateShowPersonal, nameplateShowAll or showAll) then
+                if not self.buffList[buffIndex] then
+                    self.buffList[buffIndex] = CreateFrame('Frame', nil, self, 'NameplateBuffButtonTemplate');
+                    self.buffList[buffIndex]:SetMouseClickEnabled(false);
+                    self.buffList[buffIndex].layoutIndex = buffIndex;
+                end
 
-				local buff = self.buffList[buffIndex];
+                local buff = self.buffList[buffIndex];
 
-				buff:SetID(index);
+                buff:SetID(index);
 
-				buff.Icon:SetTexture(texture);
+                buff.Icon:SetTexture(texture);
 
-				if count > 1 then
-					buff.CountFrame.Count:SetText(count);
-					buff.CountFrame.Count:Show();
-				else
-					buff.CountFrame.Count:Hide();
-				end
+                if count > 1 then
+                    buff.CountFrame.Count:SetText(count);
+                    buff.CountFrame.Count:Show();
+                else
+                    buff.CountFrame.Count:Hide();
+                end
 
-				CooldownFrame_Set(buff.Cooldown, expirationTime - duration, duration, duration > 0, DRAW_EDGE);
+                CooldownFrame_Set(buff.Cooldown, expirationTime - duration, duration, duration > 0, DRAW_EDGE);
 
-				buff:Show();
+                buff:Show();
 
-				buffIndex = buffIndex + 1;
-			end
+                buffIndex = buffIndex + 1;
+            end
 
-			index = index + 1;
+            index = index + 1;
 
-			return buffIndex > BUFF_MAX_DISPLAY;
-		end);
+            return buffIndex > BUFF_MAX_DISPLAY;
+        end);
 
-		for i = buffIndex, BUFF_MAX_DISPLAY do
-			if self.buffList[i] then
-				self.buffList[i]:Hide();
-			else
-				break;
-			end
-		end
+        for i = buffIndex, BUFF_MAX_DISPLAY do
+            if self.buffList[i] then
+                self.buffList[i]:Hide();
+            else
+                break;
+            end
+        end
 
-		if buffIndex > 1 then
-			for i = 1, buffIndex - 1 do
-				self.buffList[i]:ClearAllPoints();
+        if buffIndex > 1 then
+            for i = 1, buffIndex - 1 do
+                self.buffList[i]:ClearAllPoints();
 
-				if AURAS_DIRECTION == 1 then
-					self.buffList[i]:SetPoint('TOPLEFT', (i - 1) * (20 + (SPACING_X or 4)), 0);
-				else
-					self.buffList[i]:SetPoint('TOPRIGHT', -((i - 1) * (20 + (SPACING_X or 4))), 0);
-				end
-			end
-		end
-	end
+                if AURAS_DIRECTION == 1 then
+                    self.buffList[i]:SetPoint('TOPLEFT', (i - 1) * (20 + (SPACING_X or 4)), 0);
+                else
+                    self.buffList[i]:SetPoint('TOPRIGHT', -((i - 1) * (20 + (SPACING_X or 4))), 0);
+                end
+            end
+        end
+    end
 
-	-- self:Layout();
+    -- self:Layout();
 end
 
 local function Update(unitframe)
     unitframe.BuffFrame.ShouldShowBuff = FilterShouldShowBuff;
     unitframe.BuffFrame.UpdateBuffs    = UpdateBuffs;
-	unitframe.BuffFrame.spacing        = SPACING_X;
-	unitframe.BuffFrame.isActive       = IS_ACTIVE;
+    unitframe.BuffFrame.spacing        = SPACING_X;
+    unitframe.BuffFrame.isActive       = IS_ACTIVE;
 
     if unitframe.BuffFrame.unit and unitframe.BuffFrame.filter then
         unitframe.BuffFrame:UpdateBuffs(unitframe.BuffFrame.unit, unitframe.BuffFrame.filter, unitframe.data.unitType == 'FRIENDLY_PLAYER');
@@ -175,12 +175,12 @@ function Module:Update(unitframe)
 end
 
 function Module:UpdateLocalConfig()
-	IS_ACTIVE         = O.db.auras_is_active;
+    IS_ACTIVE         = O.db.auras_is_active;
     ENABLED           = O.db.auras_filter_player_enabled;
     BLACKLIST_ENABLED = O.db.auras_blacklist_enabled;
-	SPACING_X         = O.db.auras_spacing_x or 4;
-	DRAW_EDGE         = O.db.auras_draw_edge;
-	AURAS_DIRECTION   = O.db.auras_direction;
+    SPACING_X         = O.db.auras_spacing_x or 4;
+    DRAW_EDGE         = O.db.auras_draw_edge;
+    AURAS_DIRECTION   = O.db.auras_direction;
 
     UpdateBlacklistCache();
 end
