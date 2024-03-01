c5e4e27db5419b4be44f9d697b3b1d867254a7ac
Voopie <1324849+Voopie@users.noreply.github.com>
2024-03-01 21:58:22 +0300

    LibPlayerSpells-1.0 - druid: add dragonflight cat trash

--- Libraries/LibPlayerSpells-1.0/data/Druid.lua
+++ Libraries/LibPlayerSpells-1.0/data/Druid.lua
@@ -46,6 +46,7 @@ lib:__RegisterSpells('DRUID', 90200, 1, {
 				 209749, -- Faerie Swarm (Balance honor talent)
 				 274838, -- Feral Frenzy (Feral talent)
 				 325733, -- Adaptive Swarm (Necrolord covenant ability)
+				 405233, -- Thrash (cat) 106830
 				[201664] = 'SURVIVAL', -- Demoralizing Roar (Guardian honor talent)
 				CROWD_CTRL = {
 					[  6795] = 'TAUNT', -- Growl
@@ -289,6 +290,7 @@ lib:__RegisterSpells('DRUID', 90200, 1, {
 	[338825] =  78674, -- Primordial Arcanic Pulsar (Balance Legendary) <- Starsurge
 	[354704] = 354654, -- Grove Protection (Guardian honor talent)
 	[355315] = 323546, -- Ravenous Frenzy (Venthyr covenant ability)
+	[405233] = 106830, -- Thrash (cat)
 }, {
 	-- map aura to modified spell(s)
 	[ 16870] =   8936, -- Clearcasting (Restoration) -> Regrowth
