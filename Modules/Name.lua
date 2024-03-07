6a9f312e6e645fa5aa675e25f85296684fcb00a1
Voopie <1324849+Voopie@users.noreply.github.com>
2024-03-07 17:29:59 +0300

    Name - not needed

--- Modules/Name.lua
+++ Modules/Name.lua
@@ -876,7 +876,6 @@ function Module:StartUp()
 
         NameOnly_UpdateNameHealth(unitframe);
         NameOnly_UpdateGuildName(unitframe);
-        NameOnly_UpdateBackgroundVisibility(unitframe);
 
         UpdateAnchor(unitframe);
     end);
